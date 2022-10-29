ec6717cc06db3325a070a74a0953fe6ce9a6610c
erick <erick.sanchezcorrea@gmail.com>
2022-10-29 16:56:35 -0500

    view "descargas"

--- src/components/descargas.js
+++ src/components/descargas.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import pdf from "./Descargas/archivos-pdf/AUTORIZACION_DE_DESCUENTO_SUB_CAFAE_EP/AUTORIZACION_DE_DESCUENTO_SUB_CAFAE_EP.pdf"
-import pdfImg from "./Descargas/archivos-pdf/AUTORIZACION_DE_DESCUENTO_SUB_CAFAE_EP/1.png"
+import { useRef, useEffect,useState } from "react";
+
+import data from './Descargas/Json/archivos.json';
+import img from '../imgs/View/descargas/Archivos_Imagen-Preview/img-preview.png';
+import pdf from "./Descargas/Archivos-Downloand/AUTORIZACION_DE_DESCUENTO_SUB_CAFAE_EP.pdf"
+
+//icons extension files
+import iconPDF from '../imgs/View/descargas/icon-archivos-extension/pdf.png';
+
+
+
 
 
 const Descargas = () => {
-    console.log('Descargas');
+    const count = useRef();
+
+    useEffect(() => {
+        switch (count.current.dataset.extension) {
+            case 'pdf':;
+                count.current.src=iconPDF;
+                break;
+            default:
+                console.log(`Sorry, we are out of ${count.current.dataset.extension}.`);
+        }
+    }, []);
+
 
     return (
         <div className='container-descargas'>
+            <div>
+                <center>
+                    <h1 className='title-descargas'>
+                        DESCARGAS
+                    </h1>
+                </center>
+            </div>            
             <div className='descargas-wrapper'>
                 <div className='descargas-card'>
-                    <img src={pdfImg} alt=""/>
-                    <a download target="_blank" rel="noreferrer" href={pdf} >
-                        AUTORIZACION
-                    </a>
-                </div>
+                    <img src={data[0].ImagenPreview}alt=""  className="preview-img-pdf"/>
+                        <div className='title-preview'>
+                            {
+                                data.map((doc) =>
+                                    <div>
+                                        <img src={doc.Extension} alt="icon PDF" className='icon-extension' ref={count} data-extension={doc.Extension}/>
+                                        <a download target="_blank" rel="noreferrer" href={doc.urlArchivo}>
+                                            {doc.NombreArchivo}.{doc.Extension}
+                                        </a>
+                                    </div>
+                                ) 
+                            }
+                        </div>
 
+                    {/* <img src={pdfImg} alt=""  className="preview-img-pdf"/>
+                    <a download target="_blank" rel="noreferrer" href={pdf}>
+                        AUTORIZACION
+                    </a> */}
+                </div>
             </div>
         </div>
 
