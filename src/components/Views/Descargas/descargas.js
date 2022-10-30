92e84ba7411913e3c1b039c7478fdfba659bc010
erick <erick.sanchezcorrea@gmail.com>
2022-10-30 00:20:53 -0500

    images preview docs "Descargas"

--- src/components/Views/Descargas/descargas.js
+++ src/components/Views/Descargas/descargas.js
@@ -4,17 +4,8 @@ import { useRef, useEffect,useState } from "react";
 import data from './Json/archivos.json';
 import archivos from './Archivos-Downloand/archivos.js';
 
-// import img from '../imgs/View/descargas/Archivos_Imagen-Preview/img-preview.png';
-// import pdf from "./Descargas/Archivos-Downloand/SOLICITUD_FSRyC_VARIOS.pdf"
-
 //icons extension files
 import iconPDF from '../../../imgs/View/descargas/icon-archivos-extension/pdf.png';
-import imgPreview from '../../../imgs/View/descargas/Archivos_Imagen-Preview/img-preview.png';
-
-
-
-
-
 
 const Descargas = () => {
     // console.log(pdf)
@@ -41,24 +32,20 @@ const Descargas = () => {
                 </center>
             </div>            
             <div className='descargas-wrapper'>
-                {
-                    data.map((doc) =>
+            {
+                data.map((doc) =>
                     <div className='descargas-card'>
-                        <img src={imgPreview} alt=""  className="preview-img-pdf"/>
+                        <img src={doc.urlImgPreview} alt=""  className="preview-img-pdf"/>
                         <div className='title-preview'>
-                                <img src={iconPDF} alt="icon PDF" className='icon-extension' ref={count} data-extension={doc.Extension}/>
-                                    <a download target="_blank" rel="noreferrer" href={doc.urlArchivo}>
-                                        {doc.NombreArchivo}
-                                    </a>
-                            </div>
+                            <img src={iconPDF} alt="icon PDF" className='icon-extension' ref={count} data-extension={doc.Extension}/>
+                            <a download target="_blank" rel="noreferrer" href={doc.urlArchivo}>
+                                {doc.NombreArchivo}
+                            </a>
+                            <span class="tooltiptext">{doc.NombreArchivo}</span>
                         </div>
-
-                    ) 
-                }
-                    {/* <img src={pdfImg} alt=""  className="preview-img-pdf"/>
-                    <a download target="_blank" rel="noreferrer" href={pdf}>
-                        AUTORIZACION
-                    </a> */}
+                    </div>
+                ) 
+            }
             </div>
         </div>
 
