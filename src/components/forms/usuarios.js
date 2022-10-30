cb71cd4da83be43dec09170c5970bf2b24d3d393
Gozu3000 <erick.sanchezcorrea@gmail.com>
2022-10-29 21:13:56 -0500

    commit

--- src/components/forms/usuarios.js
+++ src/components/forms/usuarios.js
@@ -13,9 +13,6 @@ const Usuarios = () => {
     setDni(event.target.value);
   };
 
-  const handleChangeContraseña = (event) => {
-    setContraseña(event.target.value);
-  };
 
   const dispatch = useDispatch();
 
@@ -23,26 +20,19 @@ const Usuarios = () => {
     dispatch(openModal());
   };
 
-  function ingresarUsuario () {
-    let dniPrueba = 'asdf';
-    let contraseñaPrueba = '1234';
-
-    if ( dni === dniPrueba && contraseña === contraseñaPrueba ) {
-      
-    }
-  };
 
 
   return (
     <div className='usuarios-wrapper'>
         <div className='form-usuario-container'>
             <h1>Ingresa</h1>
-            <form className='form-grid'>
+            <form className='form-user'>
               <label>DNI</label>
               <input type='text' id='dni' name='dni' value={dni} onChange={handleChangeDni} />
                     
               <label>Contraseña</label>
-              <input type='text' id='contraseña' name='contraseña' value={contraseña} onChange={handleChangeContraseña} />
+              <input type='text' id='contraseña' name='contraseña' value={contraseña}  />
+              
               <button type='submit'>ingresar</button>          
             </form>
             <h3 onClick={startModal} className='links'>Registrate</h3>
