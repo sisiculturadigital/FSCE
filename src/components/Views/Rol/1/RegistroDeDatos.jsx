f0e3791361254d70763a130b0cefc9d4cd8ed1d9
Gozu3000 <erick.sanchezcorrea@gmail.com>
2022-11-21 05:19:55 -0500

    registrodedatos captura de datos

--- src/components/Views/Rol/1/RegistroDeDatos.jsx
+++ src/components/Views/Rol/1/RegistroDeDatos.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+
+const InitialValue = {
+cci: "",
+departamento: "",
+direccion: "",
+distrito: "",
+email: "",
+entidad: "",
+numeroContacto: "",
+numeroCta: "",
+provincia: ""
+}
+
 const RegistroDeDatos = () => {
+
+  const [form, setForm] = useState(InitialValue)
+
+
+  const HandleChange = (e) =>{
+    e.preventDefault()
+    setForm({
+      ...form,
+      [e.target.name ]:e.target.value
+    })
+  }
+
+  const HandleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(form)
+    setForm(InitialValue)
+
+  } 
+
+
   return (
     <div className='RegistroDeDatos'>
 
@@ -11,22 +44,25 @@ const RegistroDeDatos = () => {
         </Link>
       </div>
 
-      <form>
+      <form onSubmit={HandleSubmit}>
 
         <h2>Registro de datos</h2>
 
         <div className='total-container'> 
         
           <div className='label-container'>
-            <label htmlFor="entidad">Entidad Bancaria</label>  <input type="text" name='entidad' id='entidad' />
+            <label htmlFor="entidad">Entidad Bancaria</label>  
+            <input type="text" name='entidad' value={form.entidad} id='entidad' onChange={HandleChange} />
           </div>
 
           <div className='label-container'>
-            <label htmlFor="numeroCta">Nº Cuenta Bancaria</label> <input type="text" name='numeroCta' id='numeroCta' />
+            <label htmlFor="numeroCta">Nº Cuenta Bancaria</label> 
+            <input type="text" name='numeroCta' value={form.numeroCta} id='numeroCta'onChange={HandleChange}  />
           </div>
 
           <div className='label-container'>  
-            <label htmlFor="cci">Código Cuenta Interbancaria(CCI)</label><input type="text" name='cci' id='cci' />
+            <label htmlFor="cci">Código Cuenta Interbancaria(CCI)</label>
+            <input type="text" name='cci' value={form.cci} id='cci' onChange={HandleChange} />
           </div>
          
         </div>
@@ -35,11 +71,13 @@ const RegistroDeDatos = () => {
         <div className='total-container'> 
         
           <div className='label-container'>
-            <label htmlFor="numeroContacto">Nº de Contacto</label>  <input type="text" name='numeroContacto' id='numeroContacto' />
+            <label htmlFor="numeroContacto">Nº de Contacto</label>  
+            <input type="text" name='numeroContacto' value={form.numeroContacto} id='numeroContacto' onChange={HandleChange} />
           </div>
 
           <div className='label-container'>
-            <label htmlFor="email">Correo Electrónico</label> <input type="text" name='email' id='email' />
+            <label htmlFor="email">Correo Electrónico</label> 
+            <input type="text" name='email' value={form.email} id='email' onChange={HandleChange} />
           </div>
 
         </div>
@@ -47,30 +85,32 @@ const RegistroDeDatos = () => {
         <div className='total-container'>
 
           <div className='label-container'>
-            <label htmlFor="departamento">Departamento</label> <input type="text" name='departamento' id='departamento' />
+            <label htmlFor="departamento">Departamento</label> 
+            <input type="text" name='departamento' value={form.departamento} id='departamento' onChange={HandleChange} />
           </div>
           <div className='label-container'>
-            <label htmlFor="provincia">Provincia</label> <input type="text" name='provincia' id='provincia' />
+            <label htmlFor="provincia">Provincia</label> 
+            <input type="text" name='provincia' value={form.provincia} id='provincia' onChange={HandleChange} />
           </div>
           <div className='label-container'>
-            <label htmlFor="distrito">Distrito</label> <input type="text" name='distrito' id='distrito' />
+            <label htmlFor="distrito">Distrito</label> 
+            <input type="text" name='distrito' value={form.distrito} id='distrito' onChange={HandleChange} />
           </div>
          
         </div>
 
         <div className='total-container'>
           <div className='label-container'>
-            <label htmlFor="direccion">Dirección</label> <input type="text" name='direccion' id='direccion' />
+            <label htmlFor="direccion">Dirección</label> 
+            <input type="text" name='direccion' value={form.direccion} id='direccion' onChange={HandleChange} />
           </div>
         </div>
 
-        <div>
-          <div className='button-back-perfil button-registrar'>
-            <Link to={"/inicio"}>
-                <p>REGISTRAR</p>
-            </Link>
-          </div>
+    
+        <div className=' button-registrar-container'>
+          <input type='submit' value='REGISTRAR' className='button-registrar'/> 
         </div>
+ 
 
       </form>
 
